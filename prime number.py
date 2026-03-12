1edeb00fde70e9ba10d44194052bbe3240977926
ombadade05-hii <ombadade05@gmail.com>
2026-03-12 10:31:01 +0530

    added 9 python programs

---
+++ prime number.py
@@ -0,0 +1,12 @@
+num = int(input("Enter number: "))
+flag = False
+
+for i in range(2, num):
+    if num % i == 0:
+        flag = True
+        break
+
+if flag:
+    print("Not Prime")
+else:
+    print("Prime Number")
